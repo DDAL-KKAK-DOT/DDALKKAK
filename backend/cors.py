bcfcaded62c19f03d7ba94f20e7627f4dbba4682
haeun030 <haeun14_0_0@naver.com>
2025-05-22 23:38:24 +0900

    ♻️limited CORS

--- backend/cors.py
+++ backend/cors.py
@@ -6,7 +6,7 @@ def add_cors_middleware(app):
         CORSMiddleware,
         allow_origins=["*"],  # 실제 배포 시에는 특정 도메인으로 제한
         allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
+        allow_methods=["GET", "POST"],
+        allow_headers=["Content-Type"],
     )
     return app
